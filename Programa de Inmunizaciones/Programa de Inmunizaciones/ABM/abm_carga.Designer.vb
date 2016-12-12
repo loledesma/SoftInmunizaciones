c25e68d5e187a96880f5ef8a6b5ec46f3ccb5042
loledesma <lorenasoledadledesma@gmail.com>
2016-12-12 13:03:40 -0300

    Cambios en abm_cargo, abm_stock, abm_carga

--- Programa de Inmunizaciones/Programa de Inmunizaciones/ABM/abm_carga.Designer.vb
+++ Programa de Inmunizaciones/Programa de Inmunizaciones/ABM/abm_carga.Designer.vb
@@ -22,8 +22,264 @@ Partial Class abm_carga
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(abm_carga))
+        Dim DataGridViewCellStyle3 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle4 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.cmd_salir = New System.Windows.Forms.Button()
+        Me.cmd_limpiar = New System.Windows.Forms.Button()
+        Me.cmd_guardar = New System.Windows.Forms.Button()
+        Me.cmd_nuevo = New System.Windows.Forms.Button()
+        Me.cmd_eliminar = New System.Windows.Forms.Button()
+        Me.grp_grilla_carga = New System.Windows.Forms.GroupBox()
+        Me.dgv_carga = New System.Windows.Forms.DataGridView()
+        Me.id_carga = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.descripcion = New System.Windows.Forms.DataGridViewTextBoxColumn()
+        Me.grp_carga = New System.Windows.Forms.GroupBox()
+        Me.cmd_buscar = New System.Windows.Forms.Button()
+        Me.txt_descripcion = New System.Windows.Forms.TextBox()
+        Me.txt_id_carga = New System.Windows.Forms.TextBox()
+        Me.lbl_descripcion = New System.Windows.Forms.Label()
+        Me.lbl_carga = New System.Windows.Forms.Label()
+        Me.grp_grilla_carga.SuspendLayout()
+        CType(Me.dgv_carga, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.grp_carga.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'cmd_salir
+        '
+        Me.cmd_salir.BackColor = System.Drawing.Color.Transparent
+        Me.cmd_salir.BackgroundImage = CType(resources.GetObject("cmd_salir.BackgroundImage"), System.Drawing.Image)
+        Me.cmd_salir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.cmd_salir.FlatAppearance.BorderSize = 0
+        Me.cmd_salir.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.cmd_salir.Location = New System.Drawing.Point(339, 318)
+        Me.cmd_salir.Name = "cmd_salir"
+        Me.cmd_salir.Size = New System.Drawing.Size(60, 60)
+        Me.cmd_salir.TabIndex = 22
+        Me.cmd_salir.UseVisualStyleBackColor = False
+        '
+        'cmd_limpiar
+        '
+        Me.cmd_limpiar.BackColor = System.Drawing.Color.Transparent
+        Me.cmd_limpiar.BackgroundImage = CType(resources.GetObject("cmd_limpiar.BackgroundImage"), System.Drawing.Image)
+        Me.cmd_limpiar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.cmd_limpiar.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.cmd_limpiar.FlatAppearance.BorderSize = 0
+        Me.cmd_limpiar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkOrange
+        Me.cmd_limpiar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gold
+        Me.cmd_limpiar.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.cmd_limpiar.Location = New System.Drawing.Point(339, 214)
+        Me.cmd_limpiar.Margin = New System.Windows.Forms.Padding(4)
+        Me.cmd_limpiar.Name = "cmd_limpiar"
+        Me.cmd_limpiar.Size = New System.Drawing.Size(60, 60)
+        Me.cmd_limpiar.TabIndex = 21
+        Me.cmd_limpiar.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.cmd_limpiar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.cmd_limpiar.UseVisualStyleBackColor = False
+        '
+        'cmd_guardar
+        '
+        Me.cmd_guardar.BackColor = System.Drawing.Color.Transparent
+        Me.cmd_guardar.BackgroundImage = CType(resources.GetObject("cmd_guardar.BackgroundImage"), System.Drawing.Image)
+        Me.cmd_guardar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.cmd_guardar.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.cmd_guardar.FlatAppearance.BorderSize = 0
+        Me.cmd_guardar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkOrange
+        Me.cmd_guardar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gold
+        Me.cmd_guardar.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.cmd_guardar.Location = New System.Drawing.Point(339, 78)
+        Me.cmd_guardar.Margin = New System.Windows.Forms.Padding(4)
+        Me.cmd_guardar.Name = "cmd_guardar"
+        Me.cmd_guardar.Size = New System.Drawing.Size(60, 60)
+        Me.cmd_guardar.TabIndex = 18
+        Me.cmd_guardar.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.cmd_guardar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.cmd_guardar.UseVisualStyleBackColor = False
+        '
+        'cmd_nuevo
+        '
+        Me.cmd_nuevo.BackColor = System.Drawing.Color.Transparent
+        Me.cmd_nuevo.BackgroundImage = CType(resources.GetObject("cmd_nuevo.BackgroundImage"), System.Drawing.Image)
+        Me.cmd_nuevo.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.cmd_nuevo.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.cmd_nuevo.FlatAppearance.BorderSize = 0
+        Me.cmd_nuevo.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkOrange
+        Me.cmd_nuevo.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gold
+        Me.cmd_nuevo.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.cmd_nuevo.Location = New System.Drawing.Point(339, 10)
+        Me.cmd_nuevo.Margin = New System.Windows.Forms.Padding(4)
+        Me.cmd_nuevo.Name = "cmd_nuevo"
+        Me.cmd_nuevo.Size = New System.Drawing.Size(60, 60)
+        Me.cmd_nuevo.TabIndex = 20
+        Me.cmd_nuevo.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.cmd_nuevo.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.cmd_nuevo.UseVisualStyleBackColor = False
+        '
+        'cmd_eliminar
+        '
+        Me.cmd_eliminar.BackColor = System.Drawing.Color.Transparent
+        Me.cmd_eliminar.BackgroundImage = CType(resources.GetObject("cmd_eliminar.BackgroundImage"), System.Drawing.Image)
+        Me.cmd_eliminar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.cmd_eliminar.FlatAppearance.BorderColor = System.Drawing.Color.White
+        Me.cmd_eliminar.FlatAppearance.BorderSize = 0
+        Me.cmd_eliminar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.DarkOrange
+        Me.cmd_eliminar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Gold
+        Me.cmd_eliminar.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.cmd_eliminar.Location = New System.Drawing.Point(339, 146)
+        Me.cmd_eliminar.Margin = New System.Windows.Forms.Padding(4)
+        Me.cmd_eliminar.Name = "cmd_eliminar"
+        Me.cmd_eliminar.Size = New System.Drawing.Size(60, 60)
+        Me.cmd_eliminar.TabIndex = 19
+        Me.cmd_eliminar.TextAlign = System.Drawing.ContentAlignment.MiddleRight
+        Me.cmd_eliminar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText
+        Me.cmd_eliminar.UseVisualStyleBackColor = False
+        '
+        'grp_grilla_carga
+        '
+        Me.grp_grilla_carga.BackColor = System.Drawing.Color.Transparent
+        Me.grp_grilla_carga.Controls.Add(Me.dgv_carga)
+        Me.grp_grilla_carga.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.grp_grilla_carga.Location = New System.Drawing.Point(3, 103)
+        Me.grp_grilla_carga.Name = "grp_grilla_carga"
+        Me.grp_grilla_carga.Size = New System.Drawing.Size(308, 279)
+        Me.grp_grilla_carga.TabIndex = 17
+        Me.grp_grilla_carga.TabStop = False
+        Me.grp_grilla_carga.Text = "Existentes"
+        '
+        'dgv_carga
+        '
+        Me.dgv_carga.AllowUserToAddRows = False
+        Me.dgv_carga.AllowUserToDeleteRows = False
+        Me.dgv_carga.BackgroundColor = System.Drawing.SystemColors.ActiveCaption
+        Me.dgv_carga.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgv_carga.Columns.AddRange(New System.Windows.Forms.DataGridViewColumn() {Me.id_carga, Me.descripcion})
+        DataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle3.BackColor = System.Drawing.SystemColors.Window
+        DataGridViewCellStyle3.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle3.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        DataGridViewCellStyle3.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle3.SelectionForeColor = System.Drawing.SystemColors.Desktop
+        DataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.[False]
+        Me.dgv_carga.DefaultCellStyle = DataGridViewCellStyle3
+        Me.dgv_carga.GridColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.dgv_carga.Location = New System.Drawing.Point(9, 19)
+        Me.dgv_carga.Name = "dgv_carga"
+        Me.dgv_carga.ReadOnly = True
+        DataGridViewCellStyle4.ForeColor = System.Drawing.Color.Black
+        DataGridViewCellStyle4.SelectionForeColor = System.Drawing.Color.Black
+        Me.dgv_carga.RowsDefaultCellStyle = DataGridViewCellStyle4
+        Me.dgv_carga.Size = New System.Drawing.Size(293, 254)
+        Me.dgv_carga.TabIndex = 0
+        '
+        'id_carga
+        '
+        Me.id_carga.HeaderText = "ID"
+        Me.id_carga.Name = "id_carga"
+        Me.id_carga.ReadOnly = True
+        Me.id_carga.Width = 50
+        '
+        'descripcion
+        '
+        Me.descripcion.HeaderText = "Descripción"
+        Me.descripcion.Name = "descripcion"
+        Me.descripcion.ReadOnly = True
+        Me.descripcion.Width = 175
+        '
+        'grp_carga
+        '
+        Me.grp_carga.BackColor = System.Drawing.Color.Transparent
+        Me.grp_carga.Controls.Add(Me.cmd_buscar)
+        Me.grp_carga.Controls.Add(Me.txt_descripcion)
+        Me.grp_carga.Controls.Add(Me.txt_id_carga)
+        Me.grp_carga.Controls.Add(Me.lbl_descripcion)
+        Me.grp_carga.Controls.Add(Me.lbl_carga)
+        Me.grp_carga.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.grp_carga.Location = New System.Drawing.Point(3, 2)
+        Me.grp_carga.Name = "grp_carga"
+        Me.grp_carga.Size = New System.Drawing.Size(308, 85)
+        Me.grp_carga.TabIndex = 16
+        Me.grp_carga.TabStop = False
+        Me.grp_carga.Text = "Datos"
+        '
+        'cmd_buscar
+        '
+        Me.cmd_buscar.BackgroundImage = CType(resources.GetObject("cmd_buscar.BackgroundImage"), System.Drawing.Image)
+        Me.cmd_buscar.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.cmd_buscar.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.cmd_buscar.Location = New System.Drawing.Point(257, 11)
+        Me.cmd_buscar.Name = "cmd_buscar"
+        Me.cmd_buscar.Size = New System.Drawing.Size(32, 32)
+        Me.cmd_buscar.TabIndex = 12
+        Me.cmd_buscar.UseVisualStyleBackColor = True
+        '
+        'txt_descripcion
+        '
+        Me.txt_descripcion.Location = New System.Drawing.Point(112, 49)
+        Me.txt_descripcion.Name = "txt_descripcion"
+        Me.txt_descripcion.Size = New System.Drawing.Size(190, 20)
+        Me.txt_descripcion.TabIndex = 2
+        '
+        'txt_id_carga
+        '
+        Me.txt_id_carga.Location = New System.Drawing.Point(112, 22)
+        Me.txt_id_carga.Name = "txt_id_carga"
+        Me.txt_id_carga.Size = New System.Drawing.Size(61, 20)
+        Me.txt_id_carga.TabIndex = 0
+        '
+        'lbl_descripcion
+        '
+        Me.lbl_descripcion.AutoSize = True
+        Me.lbl_descripcion.Location = New System.Drawing.Point(6, 52)
+        Me.lbl_descripcion.Name = "lbl_descripcion"
+        Me.lbl_descripcion.Size = New System.Drawing.Size(63, 13)
+        Me.lbl_descripcion.TabIndex = 1
+        Me.lbl_descripcion.Text = "Descripción"
+        '
+        'lbl_carga
+        '
+        Me.lbl_carga.AutoSize = True
+        Me.lbl_carga.Location = New System.Drawing.Point(6, 25)
+        Me.lbl_carga.Name = "lbl_carga"
+        Me.lbl_carga.Size = New System.Drawing.Size(83, 13)
+        Me.lbl_carga.TabIndex = 0
+        Me.lbl_carga.Text = "Id Estado Carga"
+        '
+        'abm_carga
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackgroundImage = CType(resources.GetObject("$this.BackgroundImage"), System.Drawing.Image)
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.ClientSize = New System.Drawing.Size(422, 401)
+        Me.Controls.Add(Me.cmd_salir)
+        Me.Controls.Add(Me.cmd_limpiar)
+        Me.Controls.Add(Me.cmd_guardar)
+        Me.Controls.Add(Me.cmd_nuevo)
+        Me.Controls.Add(Me.cmd_eliminar)
+        Me.Controls.Add(Me.grp_grilla_carga)
+        Me.Controls.Add(Me.grp_carga)
+        Me.Name = "abm_carga"
         Me.Text = "abm_carga"
+        Me.grp_grilla_carga.ResumeLayout(False)
+        CType(Me.dgv_carga, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.grp_carga.ResumeLayout(False)
+        Me.grp_carga.PerformLayout()
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents cmd_salir As System.Windows.Forms.Button
+    Friend WithEvents cmd_limpiar As System.Windows.Forms.Button
+    Friend WithEvents cmd_guardar As System.Windows.Forms.Button
+    Friend WithEvents cmd_nuevo As System.Windows.Forms.Button
+    Friend WithEvents cmd_eliminar As System.Windows.Forms.Button
+    Friend WithEvents grp_grilla_carga As System.Windows.Forms.GroupBox
+    Friend WithEvents dgv_carga As System.Windows.Forms.DataGridView
+    Friend WithEvents grp_carga As System.Windows.Forms.GroupBox
+    Friend WithEvents cmd_buscar As System.Windows.Forms.Button
+    Friend WithEvents txt_descripcion As System.Windows.Forms.TextBox
+    Friend WithEvents txt_id_carga As System.Windows.Forms.TextBox
+    Friend WithEvents lbl_descripcion As System.Windows.Forms.Label
+    Friend WithEvents lbl_carga As System.Windows.Forms.Label
+    Friend WithEvents id_carga As System.Windows.Forms.DataGridViewTextBoxColumn
+    Friend WithEvents descripcion As System.Windows.Forms.DataGridViewTextBoxColumn
 End Class
