f9febcccb2ba3939f08257278ca9706fc6bb65f6
Joaquin38 <joaco544@gmail.com>
2017-01-06 14:15:49 -0300

    Cambio completo de menú y modificaciones en diseño de listados

--- Programa de Inmunizaciones/Programa de Inmunizaciones/My Project/Application.Designer.vb
+++ Programa de Inmunizaciones/Programa de Inmunizaciones/My Project/Application.Designer.vb
@@ -32,7 +32,7 @@ Namespace My
         
         <Global.System.Diagnostics.DebuggerStepThroughAttribute()>  _
         Protected Overrides Sub OnCreateMainForm()
-            Me.MainForm = Global.Programa_de_Inmunizaciones.Menú
+            Me.MainForm = Global.Programa_de_Inmunizaciones.Menu
         End Sub
     End Class
 End Namespace
