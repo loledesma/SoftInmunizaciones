8475fde96a13a61b822819a708b882b4591a2998
loledesma <lorenasoledadledesma@gmail.com>
2017-03-29 10:05:17 -0300

    ACTUALIZACION

--- Programa de Inmunizaciones/Programa de Inmunizaciones/Modulos/global1.vb
+++ Programa de Inmunizaciones/Programa de Inmunizaciones/Modulos/global1.vb
@@ -2,5 +2,7 @@
 Module global1
     Public acceso As New acceso_datos With {._motor = acceso_datos.motores.sqlServer _
                                         , ._cadena_conexion = "Provider=SQLNCLI11;Data Source=25.36.109.252;Persist Security Info=True;User ID=LORE;Initial Catalog=INMUNIZACIONES;password = lore88"} 'VER CADENA DE CONEXION DEFINITIVA
+    Public acceso2 As New acceso_datos With {._motor = acceso_datos.motores.sqlServer _
+                                       , ._cadena_conexion = "Provider=SQLNCLI11;Data Source=LORE-PC\SQLEXPRESS;Persist Security Info=True;User ID=LORE;Initial Catalog=INMUNIZACIONES;password = lore88"} 'VER CADENA DE CONEXION DEFINITIVA
 End Module
 
