d6e1eb7ce19997f7c4636bd95128760efadf85a9
Joaquin38 <joaco544@gmail.com>
2017-06-06 12:14:48 -0300

    Implementado ToolTip buscador en notificaciones

--- Programa de Inmunizaciones/Programa de Inmunizaciones/Funciones/Registrar_notificaciones.vb
+++ Programa de Inmunizaciones/Programa de Inmunizaciones/Funciones/Registrar_notificaciones.vb
@@ -659,4 +659,21 @@
     Private Sub rdio_hoy_CheckedChanged(sender As Object, e As EventArgs) Handles rdio_hoy.CheckedChanged
         cargar_grilla()
     End Sub
+
+    Private Sub txt_efectores_TextChanged(sender As Object, e As EventArgs) Handles txt_efectores.TextChanged
+        Dim sql As String = ""
+        Dim tabla As New DataTable
+        Dim efectores As String = ""
+        sql &= "SELECT  EF.nombre as nombre "
+        sql &= " FROM EFECTORES EF "
+        sql &= " WHERE EF.nombre LIKE '%" & txt_efectores.Text & "%'"
+        tabla = acceso.consulta(sql)
+
+        Dim c As Integer = 0
+        For c = 0 To tabla.Rows.Count - 1
+            efectores += tabla.Rows(c)("nombre") & vbCrLf
+        Next
+
+        tltp_notificaciones.SetToolTip(txt_efectores, efectores)
+    End Sub
 End Class
