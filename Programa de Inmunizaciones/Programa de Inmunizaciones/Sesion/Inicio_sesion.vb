e424b445dd91ba79603cb7459ffbada909e64240
loledesma <lorenasoledadledesma@gmail.com>
2017-09-20 17:39:01 -0300

    CONFIRMAR

--- Programa de Inmunizaciones/Programa de Inmunizaciones/Sesion/Inicio_sesion.vb
+++ Programa de Inmunizaciones/Programa de Inmunizaciones/Sesion/Inicio_sesion.vb
@@ -9,6 +9,9 @@
     ' como el nombre de usuario, nombre para mostrar, etc.
 
     Private Sub OK_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK.Click
+        If name.Text = "" Then
+            MsgBox("Debe ingresar un usuario")
+        End If
         Dim samplePrincipal As New SampleIPrincipal(
     Me.name.Text, Me.password.Text)
         Me.password.Text = ""
