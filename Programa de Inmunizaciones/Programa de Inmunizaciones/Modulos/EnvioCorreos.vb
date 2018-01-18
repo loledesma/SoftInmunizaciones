e402f286eb8deca9d33c4e19553558bfe0847a5a
loledesma <lorenasoledadledesma@gmail.com>
2018-01-18 12:13:29 -0300

    CONFIRMAR

--- Programa de Inmunizaciones/Programa de Inmunizaciones/Modulos/EnvioCorreos.vb
+++ Programa de Inmunizaciones/Programa de Inmunizaciones/Modulos/EnvioCorreos.vb
@@ -1,5 +1,7 @@
 ﻿Imports System.Net
 Imports System.Net.Mail
+Imports System.Net.Mime
+Imports System.Text
 
 Module Correo
 
@@ -10,13 +12,30 @@ Module Correo
     Sub enviarCorreo(ByVal destinatario As String)
         Try
             correos.To.Clear()
-            correos.Body = "Desde el equipo de sistemas esperamos que pases un excelente día y que tengas un excelente cumpleaños!"
-            correos.Subject = "¡FELIZ CUMPLE!"
             'correos.Body = mensaje
             'correos.Subject = asunto
+            correos.From = New MailAddress("soft.inmunizaciones@gmail.com")
             correos.IsBodyHtml = True
             correos.To.Add(Trim(destinatario))
+            correos.Subject = "¡FELIZ CUMPLE!"
             'Dim imagen = New System.Drawing.Bitmap()
+
+            Dim text As String = "Desde el equipo de sistemas esperamos que pases un excelente día y que tengas un excelente cumpleaños!"
+
+            Dim plainView As AlternateView = AlternateView.CreateAlternateViewFromString(text, Encoding.UTF8, MediaTypeNames.Text.Plain)
+
+            Dim html As String = "<img src='cid:imagen' />"
+
+            Dim htmlView As AlternateView = AlternateView.CreateAlternateViewFromString(html, Encoding.UTF8, MediaTypeNames.Text.Html)
+
+            Dim img As LinkedResource = New LinkedResource("C:\Users\LORE\Documents\GitHub\SoftInmunizaciones\Programa de Inmunizaciones\Programa de Inmunizaciones\Resources\cumpleaños.jpg", MediaTypeNames.Image.Jpeg)
+            img.ContentId = "cumpleaños"
+
+            htmlView.LinkedResources.Add(img)
+
+            correos.AlternateViews.Add(plainView)
+            correos.AlternateViews.Add(htmlView)
+
 
             'Dim ruta = Programa_de_Inmunizaciones.My.Resources.Cartel_cumpleaños.ToString
             'If ruta <> "" Then
@@ -24,14 +43,14 @@ Module Correo
             '    correos.Attachments.Add(archivo)
             'End If
 
-            Dim ruta As String = Application.StartupPath & "\Resources\cumpleaños.jpg"
-            Dim archivo As Net.Mail.Attachment = New Net.Mail.Attachment(ruta)
-            correos.Attachments.Add(archivo)
+            'Dim ruta As String = Application.StartupPath & "\Resources\cumpleaños.jpg"
+            'Dim archivo As Net.Mail.Attachment = New Net.Mail.Attachment(ruta)
+            'correos.Attachments.Add(archivo)
 
-            correos.From = New MailAddress("soft.inmunizaciones@gmail.com")
+
             envios.Credentials = New NetworkCredential("soft.inmunizaciones@gmail.com", "Inmunizaciones2015")
 
-            'Datos importantes no modificables para tener acceso a las cuentas
+            ''Datos importantes no modificables para tener acceso a las cuentas
 
             envios.Host = "smtp.gmail.com"
             envios.Port = 587
